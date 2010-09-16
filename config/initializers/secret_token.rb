3c4c57da97d3cfb45deb9138ce4bb1cfe606c34c
Alan Elliott <alan@ils-bc.com>
2010-09-16 15:47:41 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'a1abb2ed8798ab4ae5de112b813dc31f56d7a8bc9e97bee1c4f32c4b8cf8984836791d280f5aa91cc755e512c6ab8031ba66a1a0b18d8cde6a9784988f2d2fd9'
