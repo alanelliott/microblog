37cfdf6085efce1c98a9f61b534b966fdc7eb4a6
Alan Elliott <alan@ils-bc.com>
2010-09-29 11:10:26 -0400

    finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Microblog::Application.routes.draw do
+  get "users/new"
+  match '/signup',  :to => 'users#new'
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
